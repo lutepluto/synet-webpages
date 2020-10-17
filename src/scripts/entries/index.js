62e81be6acd8a7167fa0bbebd96cc15b11b02b2d
lutepluto <lutepluto@gmail.com>
2020-10-17 16:53:53 +0800

    add footer

--- src/scripts/entries/index.js
+++ src/scripts/entries/index.js
@@ -21,3 +21,22 @@ $(document).on('click', '[data-toggle="consultantIndicator"] >li', e => {
   $panel.parent().children('.active').removeClass('active');
   $panel.addClass('active');
 });
+
+// let ticking = false;
+// const viewportHeight = Math.max(window.innerHeight || 0, document.documentElement.clientHeight || 0);
+// $(window).on('scroll resize', e => {
+//   if (!ticking) {
+//     ticking = true;
+//     const topbar = document.querySelector('.topbar');
+//     if (topbar) {
+//       const { bottom } = topbar.getBoundingClientRect();
+//       console.log(viewportHeight, bottom, viewportHeight - bottom);
+//       if (viewportHeight <= bottom) {
+//         $(topbar).addClass('topbar--bottom-fixed');
+//       } else {
+//         $(topbar).removeClass('topbar--bottom-fixed');
+//       }
+//     }
+//     ticking = false;
+//   }
+// });
